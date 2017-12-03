5733b9473899635d8d83ef848ed11dd8316cb40f
Jessy Phaëton <phaeton.jessy@icloud.com>
2017-12-03 15:42:50 +0100

    création de la partie entête commune à toutes les vues

---
+++ view/_head.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>article</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="view/assets/styleSheetAccueil.css">
+    <link href="https://fonts.googleapis.com/css?family=Contrail+One|Open+Sans" rel="stylesheet">
+  </head>
+  <body>
