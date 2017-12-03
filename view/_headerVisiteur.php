a63345ecd65a5385dc3143d1a29719ace09b99a3
Jessy Phaëton <phaeton.jessy@icloud.com>
2017-12-03 15:43:34 +0100

    création du header visiteur pour la vue accueil visiteur.

---
+++ view/_headerVisiteur.php
@@ -0,0 +1,4 @@
+<header>
+  <h1>Billet simple pour l'Alaska</h1>
+  <p>Bienvenu sur mon blog, je suis Jean Forteroche un passionné de voyage.<br/>A travers ce blog je vous partagerais l'une de mes expériences passées lors de mon voyage inattendu en Alaska.</p>
+</header>
