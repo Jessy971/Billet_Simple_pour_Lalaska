41f67012f2a0907ed8ddd4d9acb3bc3c891f8409
Jessy Phaëton <phaeton.jessy@icloud.com>
2017-12-05 12:09:27 +0100

    Affiche la vue correspondante en fonction de si on se trouve sur le panel administrateur ou l'accueil visiteur. Suppression également de code inutile.

--- controller/listeArticlesController.php
+++ controller/listeArticlesController.php
@@ -7,6 +7,30 @@ $bdd = new DataConnection();
 $manager = new ManagerArticle($bdd->bdd());
 
 $articles = $manager->readAll(50,0);
-$arts = [];
+$css = 'rel="stylesheet" href="view/assets/styleSheetAccueil.css"';
 
-include_once 'view/viewAccueilVisiteur.php';
+if(isset($_POST['pseudo'],$_POST['password']))
+{
+  $login = htmlspecialchars($_POST['pseudo']);
+  $password = htmlspecialchars($_POST['password']);
+
+  if($login == 'admin' && $password == 'admin')
+  {
+    $titre = 'Administrateur';
+    $btnManager ='view/__btnManager.php';
+    include_once('view/viewAccueilAdmin.php');
+
+  }
+  else
+  {
+    echo 'mot de passe  ou identifiant incorrecte.';
+  }
+}
+else {
+  $titre = 'Accueil';
+  $btnManager = 'view/__btnLireSuite.php';
+  include_once('view/viewAccueilVisiteur.php');
+
+
+
+}
