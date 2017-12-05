e004a3b4c69cb2c542946de71757f7327a52e77d
Jessy Phaëton <phaeton.jessy@icloud.com>
2017-12-05 23:02:14 +0100

    formulaire de commentaires

---
+++ view/__formCommentaire.php
@@ -0,0 +1,5 @@
+<form method="POST" action="articleVisiteur.php">
+  <input type="text" name="pseudo" id="speudo" placeholder="Votre pseudo">
+  <textarea name="commentaire" id="commentaire" placeholder="Ecrivez votre commentaire ici"></textarea>
+  <input id="submit" type="submit" value="Poster">
+</form>
