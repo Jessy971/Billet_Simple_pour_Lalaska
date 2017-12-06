7d085dc32e0123765d7d6b977da8f11260d19025
Jessy Phaëton <phaeton.jessy@icloud.com>
2017-12-06 14:26:19 +0100

    ajout dun lien pour fontawesome

--- view/_head.php
+++ view/_head.php
@@ -5,5 +5,6 @@
     <meta charset="utf-8">
     <link <?php echo $css;?> >
     <link href="https://fonts.googleapis.com/css?family=Contrail+One|Open+Sans" rel="stylesheet">
+    <script src="https://use.fontawesome.com/cc1d85f612.js"></script>
   </head>
   <body>
