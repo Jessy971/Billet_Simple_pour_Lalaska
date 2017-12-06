79772e4d15787c3860cef3edd3f08cf642d6c591
Jessy Phaëton <phaeton.jessy@icloud.com>
2017-12-06 14:25:20 +0100

    modification du chemin de l'attribut action

--- view/__formCommentaire.php
+++ view/__formCommentaire.php
@@ -1,4 +1,4 @@
-<form method="POST" action="articleVisiteur.php">
+<form method="POST" action="newCommentaireController.php">
   <input type="text" name="pseudo" id="speudo" placeholder="Votre pseudo">
   <textarea name="commentaire" id="commentaire" placeholder="Ecrivez votre commentaire ici"></textarea>
   <input id="submit" type="submit" value="Poster">
