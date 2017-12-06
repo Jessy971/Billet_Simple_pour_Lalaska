628bf08a8810bc7a2e5bc1f7126f8ba445c07dc9
Jessy Phaëton <phaeton.jessy@icloud.com>
2017-12-06 02:15:53 +0100

    suppression de code inutile

--- controller/getArticleController.php
+++ controller/getArticleController.php
@@ -1,33 +1,35 @@
 <?php
-/*if(isset($_SESSION['password']) && isset($_SESSION['login'])){
-  require_once('../../modele/class/ManagerArticle.class.php');
-  require_once('../../controller/admin/php/admin.php');
-  $titre = 'Editeur';
-
-  if(isset($_GET['id'])){
-    $_SESSION['idArticle'] = htmlspecialchars($_GET['id']);
-    $article = new Articles();
-    $contenu = $article->one($_GET['id']);
-  }
-}
-else
-{
-    echo "L'accès à cette page ne vous est pas autorisé!";
-}*/
-
+session_start();
 require_once('../modele/class/ManagerArticle.class.php');
 require_once('../modele/class/ManagerCommentaires.class.php');
 require_once('../modele/bddConnexionClass.php');
-/*require_once('../controller/admin/php/admin.php');*/
 
-if(isset($_GET['id'])){
+if(isset($_GET['id']))
+{
+
   $titre        = 'article';
   $css          = 'rel="stylesheet" href="../view//assets/styleSheetArticle.css"';
   $idArticle    = intVal($_GET['id']);
   $bdd          = new DataConnection();
   $article      = new ManagerArticle($bdd->bdd());
   $contenu      = $article->selectOne($idArticle);
-  $coms         = new ManagerCommentaires($bdd->bdd());
-  /*$commentaires = $coms -> readAllComments($idArticle, 50, 0);*/
+  $com          = new ManagerCommentaires($bdd->bdd());
+  $commentaires = $com->readAllComments($idArticle, 50, 0);
+
+  /**
+  * Si il existe des variables de session cela veut dire que nous sommes authentifier.
+  * Nous pouvons donc avoir accès à la vue permettant de supprimer les commentaires.
+  * En configurant les variables correspondante.
+  **/
+  if(isset($_SESSION['password'], $_SESSION['login']))
+  {
+    $accueil      = '../';
+    $scriptJs     ='type="text/javascript" src="js/set_btn_hide_sign.js"';
+  }
+  else
+  {
+    $accueil      = '../';
+    $scriptJs     ='type="text/javascript" src="js/set_btn.js"';
+  }
   include_once('../view/viewArticle.php');
 }
