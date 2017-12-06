5df1e191d3ce075368508712404385877bf60272
Jessy Phaëton <phaeton.jessy@icloud.com>
2017-12-06 14:19:52 +0100

    arrangemant du code.

--- controller/EditerArticleController.php
+++ controller/EditerArticleController.php
@@ -1,14 +1,14 @@
 <?php
 session_start();
 setcookie('idArticle', intVal($_GET['id']),0, null, null, false, true);
-if(isset($_SESSION['password'], $_SESSION['login']))
-
 $css="";
+if(isset($_SESSION['password'], $_SESSION['login']))
 {
   if(isset($_GET['id']) && $_GET['id'] != 0)
   {
     require_once('../modele/bddConnexionClass.php');
     require_once('../modele/class/ManagerArticle.class.php');
+
     $idArticle = intval($_GET['id']);
     $bdd = new DataConnection();
     $article = new ManagerArticle($bdd->bdd());
