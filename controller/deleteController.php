94452ca199f6279a948197e66b12c06b333a2d25
Jessy Phaëton <phaeton.jessy@icloud.com>
2017-12-06 14:20:29 +0100

    suppression de code inutile

--- controller/deleteController.php
+++ controller/deleteController.php
@@ -5,14 +5,27 @@ if(isset($_SESSION['password'], $_SESSION['login']))
 
   require_once('../modele/bddConnexionClass.php');
   require_once('../modele/class/ManagerArticle.class.php');
+  require_once('../modele/class/ManagerCommentaires.class.php');
 
   $bdd = new DataConnection();
-  $article = new ManagerArticle($bdd->bdd());
+
   if(isset($_GET['id']))
   {
+    $article = new ManagerArticle($bdd->bdd());
     $idArticle = intVal($_GET['id']);
     $article->delete($idArticle);
     header("Location: ../index.php");
   }
+  elseif(isset($_GET['idCom']))
+  {
+    $idCom = intVal($_GET['idCom']);
+    $commantaire = new ManagerCommentaires($bdd->bdd());
+    $commantaire->delete($idCom);
+    /*header("Location: ../index.php");*///redirege sur l'article en cour de lecture.
+    echo $idCom;
+    echo '<br>commentaires supprimé.';
+  }
 }
-header("Location: ../index.php");
+else {
+  header("Location: ../index.php");
+}
