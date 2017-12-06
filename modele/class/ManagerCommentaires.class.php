63e85479bab9f22a57dbe9616bd2e38ab1444191
Jessy Phaëton <phaeton.jessy@icloud.com>
2017-12-06 14:24:38 +0100

    réorganisation du code et suppression de partie inutile

--- modele/class/ManagerCommentaires.class.php
+++ modele/class/ManagerCommentaires.class.php
@@ -1,4 +1,5 @@
 <?php
+
   class ManagerCommentaires extends ManagerArticle{
     public function __construct($bd)
     {
@@ -6,61 +7,47 @@
     }
 
     //cree un nouveau commentaire
-    public function createCom($pseudo, $com, $idArt){
+    public function createCom($pseudo, $com, $idArt)
+    {
         $creat= $this->connexionBdd->prepare(' INSERT INTO commentaires(pseudo, commentaire, date_creation, id_article) VALUES(:pseudo, :com, NOW(), :idArt)');
-        $creat->execute(array(
-          'pseudo' => $pseudo,
-          'com' => $com,
-          'idArt' => $idArt));
+        $creat->execute(
+          array(
+            'pseudo' => $pseudo,
+            'com'    => $com,
+            'idArt'  => $idArt)
+        );
     }
 
-    //Affiche tout les articles
-    public function readAllComments($idArticle, $limt, $offst){
+    //Affiche tout les commentaires.
+    public function readAllComments($idArticle, $limt, $offst)
+    {
         $read = $this->connexionBdd->prepare('SELECT id, pseudo, commentaire, signale, DATE_FORMAT(date_creation, \'%d-%m-%Y à %Hh%i.\') AS date_com FROM commentaires WHERE id_article = :id_article ORDER BY id DESC LIMIT '.$offst.','.$limt);
         $read->execute(array(
           'id_article'=> $idArticle));
-          while ($donnees = $read->fetch()) {
-            if($donnees['signale'] != 1){
-              echo '<div>
-                    <p class="pseudo"> Posté par : ' . $donnees['pseudo'] . '</p>
-                    <p class="commentaires">' . $donnees['commentaire'] . '</p>
-                    <p class="datetime">Le ' . $donnees['date_com'] . '</p>
-                    <a class="supr" href="article.php?idCom='.$donnees['id'].'"><button>Supprimer</button></a>
-                    <a class="lienBtn" href="articleVisiteur.php?idComsign=' .$donnees['id']. '"><button class="btn">Signaler</button></a>
-                  </div>';
-            }
-            else {
-              echo '<div>
-                    <p class="pseudo"> Posté par : ' . $donnees['pseudo'] . '</p>
-                    <p class="commentaires"> ' . $donnees['commentaire'] . ' <i class="fa fa-exclamation-triangle" aria-hidden="true"></i></p>
-                    <p class="datetime">Le ' . $donnees['date_com'] . '</p>
-                    <a class="supr" href="article.php?idCom='.$donnees['id'].'"><button>Supprimer</button></a>
-                    <a class="lienBtn" href="articleVisiteur.php?idComsign=' .$donnees['id']. '"><button class="btn">Signaler</button></a>
-                  </div>';
-            }
-          }
         return $read;
       }
 
     //Signale un commentaire pour le modérer.
-    public function signaleComment($idCom, $signaleCom){
-
+    public function signaleComment($idCom, $signaleCom)
+    {
       $signale = $this->connexionBdd->prepare('UPDATE commentaires SET signale = :signaleCom WHERE id = :idCom');
-      $signale->execute(array(
-        'signaleCom' => $signaleCom,
-        'idCom' => $idCom
-      ));
-
+      $signale->execute(
+        array(
+          'signaleCom' => $signaleCom,
+          'idCom'      => $idCom)
+      );
     }
 
-    //supression d'article
-    public function delete($id){
+    //supression du commentaire.
+    public function delete($id)
+    {
       $delete=$this->connexionBdd->prepare('DELETE FROM commentaires WHERE id=?');
       $delete->execute(array($id));
     }
 
     //récupère le nombre de commentaires dans la table
-    public function countCom($idArticle){
+    public function countCom($idArticle)
+    {
       $count = $this->connexionBdd->prepare('SELECT id FROM commentaires WHERE id_article = :idArticle');
       $count -> execute(['idArticle' => $idArticle]);
       $nCom = $count->rowCount();
