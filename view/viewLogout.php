f129a5a5110d5d43b862fd2b0754b2635ec5ea13
Jessy Phaëton <phaeton.jessy@icloud.com>
2017-12-06 14:27:42 +0100

    remplacement de logout par viewLogout

---
+++ view/viewLogout.php
@@ -0,0 +1,10 @@
+<?php
+  include_once('../view/_head.php');
+?>
+        <section>
+            <h1>Déconnecté</h1>
+            <p>Vous avez été déconnecté avec succès.</p>
+            <a href="../index.php"><button id="pAccueilBtn">Page d'accueil</button></a>
+        </section>
+    </body>
+</html>
