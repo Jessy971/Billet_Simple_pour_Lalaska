c455c859340add7b16e834aaba4b305185b37016
Jessy Phaëton <phaeton.jessy@icloud.com>
2017-12-06 02:21:19 +0100

    modif du chemin pour la création d'un nouvelle article

--- view/__menu.php
+++ view/__menu.php
@@ -2,7 +2,7 @@
     <header id="menu" role="navigation">
       <ul>
         <li><span id="date_heure"></span></li>
-        <li><a href="../../../view/admin/newArticle.php?id=0"><button>Nouveau</button></a></li> <!--btn nouveau-->
+        <li><a href="controller/EditerArticleController.php?id=0"><button>Nouveau</button></a></li> <!--btn nouveau-->
         <li><a href="../../../view/admin/logout.php"><button>Deconnexion</button></a></li> <!--btn deconnexion-->
       </ul>
     </header>
