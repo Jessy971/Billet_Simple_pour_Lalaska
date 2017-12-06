62175d0c131cdb80e030073e4e381fdbc37067fb
Jessy Phaëton <phaeton.jessy@icloud.com>
2017-12-06 14:25:50 +0100

    suppression de code répétif.

--- view/__menu.php
+++ view/__menu.php
@@ -1,9 +1,8 @@
-<!DOCTYPE html>
-    <header id="menu" role="navigation">
-      <ul>
-        <li><span id="date_heure"></span></li>
-        <li><a href="controller/EditerArticleController.php?id=0"><button>Nouveau</button></a></li> <!--btn nouveau-->
-        <li><a href="../../../view/admin/logout.php"><button>Deconnexion</button></a></li> <!--btn deconnexion-->
-      </ul>
-    </header>
-    <script src="controller/js/date_time.js"></script>
+<header id="menu" role="navigation">
+  <ul>
+    <li><span id="date_heure"></span></li>
+    <li><a href="controller/EditerArticleController.php?id=0"><button>Nouveau</button></a></li> <!--btn nouveau-->
+    <li><a href="controller/logoutController.php"><button>Deconnexion</button></a></li> <!--btn deconnexion-->
+  </ul>
+</header>
+<script src="controller/js/date_time.js"></script>
