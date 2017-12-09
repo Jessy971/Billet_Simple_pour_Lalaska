c5dbce0a982e680c778baa908b6743c6fef65a13
Jessy Phaëton <phaeton.jessy@icloud.com>
2017-12-09 22:53:18 +0100

    suppression de code inutile et modification du chemin pour l'action d'enregistrement d'un article

--- view/viewEditer.php
+++ view/viewEditer.php
@@ -3,8 +3,8 @@ include_once('_head.php');
  ?>
     <h1>Editeur de Texte</h1>
 
-    <form method="POST" action="enregisterArticleController.php">
-        <textarea name="newArticle">
+    <form method="POST" action="../article/enregister">
+        <textarea name = "newArticle">
           <?php
             if(isset($contenu)){
               foreach ( $contenu as $article) {
@@ -18,7 +18,7 @@ include_once('_head.php');
     <form method="POST" action="../../accueilAdmin">
         <input type="submit" value="Accueil">
     </form>
-    <script type="text/javascript" src="../../lib/tinymce/tinymce.min.js"></script>
-    <script type="text/javascript" src="../../controller/js/textEditor.js"></script>
+    <script type="text/javascript" src="../lib/tinymce/tinymce.min.js"></script>
+    <script type="text/javascript" src="../controller/js/textEditor.js"></script>
   </body>
 </html>
