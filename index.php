bad45011daadf3346b9ca53a206879056d0727ee
Jessy Phaëton <phaeton.jessy@icloud.com>
2017-12-09 22:46:28 +0100

    Ajout de route pour correspondre aux actions qui peuvent etre la page d'accueil coté visiteur ou administrateur. la lecture d'article et l'affichage de commentaires etc.

--- index.php
+++ index.php
@@ -5,38 +5,53 @@ session_start();
 echo '<pre>';
 echo $_SERVER['REQUEST_URI'];
 echo '<pre>';
-include('controller/listeArticlesController.php');*/
+*/
+
 $url = '';
 if(isset($_GET['url']))
 {
   $url = explode('/',htmlspecialchars($_GET['url']));
 }
-/*var_dump($url);*/
+/*echo'<pre>';
+var_dump($url);
+echo'<pre>';*/
 //Redirige sur la page d'accueil visiteur.
 if(empty($url))
 {
   include_once('controller/listeArticlesController.php');
-
 }
+
 //Redirige sur la page de connexion administrateur.
 elseif($url[0] == 'login')
 {
   include_once('controller/loginController.php');
 }
+
 //Redirige sur la page d'accueil administrateur.
-elseif($url[0] =='adminpage' || $url[0] =='accueilAdmin')
+elseif($url[0] =='accueilAdmin')
 {
   include_once('controller/listeArticlesController.php');
 }
+
 //Redirige sur la page de l'article choisi.
 elseif($url[0] == 'article' && !empty($url[1]))
 {
-
   include_once('controller/getArticleController.php');
 }
+
 //Redirige sur la page d'accueil viiteur.
-elseif($url[0] =='editer' && !empty($url['2']))
+elseif($url[0] == 'Editer' && $url[1] == 'new')
 {
-  
   include_once('controller/EditerArticleController.php');
+}
+
+elseif ($url == 'logout')
+{
+  include_once('controller/logoutController.php');
+}
+
+elseif ($url[0] == 'article' && $url[1] == 'enregister')
+{
+  echo $_POST['newArticle'];
+  include_once('controller/enregisterArticleController.php');
 }
