926f2100446ff0f90e4c2b1ff3d11d55a6c83851
Jessy Phaëton <phaeton.jessy@icloud.com>
2017-12-09 22:51:11 +0100

    modification des routes pour les actions correspondante à la création d'un nouvel article et la déconnexion la partie administrateur

--- view/__menu.php
+++ view/__menu.php
@@ -1,8 +1,8 @@
 <header id="menu" role="navigation">
   <ul>
     <li><span id="date_heure"></span></li>
-    <li><a href="controller/EditerArticleController.php?id=0"><button>Nouveau</button></a></li> <!--btn nouveau-->
-    <li><a href="controller/logoutController.php"><button>Deconnexion</button></a></li> <!--btn deconnexion-->
+    <li><a href="Editer/new"><button>Nouveau</button></a></li> <!--btn nouveau-->
+    <li><a href="logout"><button>Deconnexion</button></a></li> <!--btn deconnexion-->
   </ul>
 </header>
 <script src="controller/js/date_time.js"></script>
