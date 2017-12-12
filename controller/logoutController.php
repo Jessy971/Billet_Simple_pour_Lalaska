93ab6cc6ef600a8b62eb5fdadda0043c5bdf012d
Jessy Phaëton <phaeton.jessy@icloud.com>
2017-12-12 13:37:10 +0100

    ménage

--- controller/logoutController.php
+++ controller/logoutController.php
@@ -1,6 +1,5 @@
 <?php
 $titre = 'Déconnexion';
 $css = 'rel="stylesheet" href="view/assets/styleSheetLogout.css"';
-/*setcookie('authentifier',"",time()+1, null, null, false, true);*/
 session_destroy();
 include_once('view/viewLogout.php');
